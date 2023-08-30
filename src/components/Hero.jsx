25850ad326c7d5501a299be262939426e28e0ea0
Musaddiq <85467906+musaddiq-codes@users.noreply.github.com>
2023-08-30 20:54:40 +0500

    R

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,7 +20,7 @@ const Hero = () => {
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I develop 3D visuals, user <br className='sm:block hidden' />
-            interfaces and web applications
+            interfaces and MERN Stack Solutions.
           </p>
         </div>
       </div>
