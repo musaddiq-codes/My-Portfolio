392550bcb023e29491908fa5ffe16a1785737a5a
Musaddiq <85467906+musaddiq-codes@users.noreply.github.com>
2023-09-02 13:58:12 +0500

    Contacts

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter } from "react-router-dom";
 
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
-
+  
 const App = () => {
   return (
     <BrowserRouter>
@@ -14,10 +14,10 @@ const App = () => {
         <Experience />
         <Tech />
         <Works />
-        <Feedbacks />
+        {/* <Feedbacks /> */}
         <div className='relative z-0'>
           <Contact />
-          <StarsCanvas />
+          {/* <StarsCanvas /> */}
         </div>
       </div>
     </BrowserRouter>
